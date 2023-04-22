e07921d6c9bc6f2fa798c9082ec9ff43ff215440
Anton <asiuraev@parseq.pro>
2023-04-23 00:14:47 +0800

    Added image generation

--- index.js
+++ index.js
@@ -14,41 +14,63 @@ const openai = new OpenAIApi(configuration)
 
 bot.start((ctx) => ctx.reply(phrases.wellcome))
 
+bot.command('help', async (ctx) => {
+  ctx.reply(phrases.help)
+})
+
 bot.on('message', async (ctx) => {
   try {
     const inputText = ctx.message.text
     const prompt = messageForBot(inputText)
 
-    if(prompt) {
-      await sendAndReceive(prompt, ctx)
+    if(prompt && prompt.text && prompt.type) {
+      switch (prompt.type) {
+        case 'text':
+          await generateText(prompt.text, ctx)
+          break
+        case 'image':
+          await generateImg(prompt.text, ctx)
+          break
+      }
     }
   } catch (error) {
     ctx.reply(phrases.reject)
-    console.log(error)
+    console.error(error)
   }
 })
 
 const messageForBot = (message) => {
-  let keyword = ""
-
   if(!message) {
-    return ""
+    return null
   }
 
-  keywords.forEach(word => {
-    if (message.startsWith(`${word}, `)) {
-      keyword = word
-    }
-  })
+  const { keyword, type } = getPromptTypeFromMessage(message)
 
-  if(keyword) {
-    return message.slice(`${keyword}, `.length)
+  if(keyword && type) {
+    const text = message.slice(`${keyword},`.length)
+    return { text, type }
   } else {
-    return ""
+    return null
   }
 }
 
-const sendAndReceive = async (message, ctx) => {
+const getPromptTypeFromMessage = (message) => {
+  let keyword = ""
+  let promptType = ""
+
+  for (const key in keywords) {
+    keywords[key].forEach(word => {
+      if (message.startsWith(`${word},`)) {
+        keyword = word
+        promptType = key
+      }
+    })
+  }
+
+  return { keyword, type: promptType }
+}
+
+const generateText = async (message, ctx) => {
   try {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
@@ -59,9 +81,24 @@ const sendAndReceive = async (message, ctx) => {
       frequency_penalty: 0.0,
       presence_penalty: 0.6,
       stop: ["AI", "Human"],
-    });
+    })
     const outputText = response.data.choices[0].text
     ctx.reply(outputText)
+  } catch (error) {
+    ctx.reply(phrases.reject)
+    console.error(error)
+  }
+}
+
+const generateImg = async (message, ctx) => {
+  try {
+    const response = await openai.createImage({
+      prompt: message,
+      n: 1,
+      size: "512x512",
+    })
+    const image_url = response.data.data[0].url
+    ctx.replyWithPhoto({ url: image_url })
   } catch (error) {
     ctx.reply(phrases.reject)
     console.error(error)
